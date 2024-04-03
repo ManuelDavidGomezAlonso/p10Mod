d4032aa2d42500e1bb6ae4e8f4809a9160695695
ManuelDavidGomezAlonso <alu0101347301@ull.edu.es>
2024-04-03 16:09:08 +0000

    Leve corrección

--- src/client.ts
+++ src/client.ts
@@ -5,7 +5,7 @@ const client = net.connect({port: 60300});
   client.on('data', (dataJSON) => {
     const data = JSON.parse(dataJSON.toString());
     if (data.command === "wc") {
-      console.log(`Number of Lines in ${process.argv[2]}: ${data.data}`);
+      console.log(`\nNumber of Lines in ${process.argv[2]}: ${data.data}\n`);
     } else {
       console.log("file, not found");
     }
